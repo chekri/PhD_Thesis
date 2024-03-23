f3222bf7b527b23b7550dc544086acacda2cd1b2
chakri <chekri.dsp@gmail.com>
2024-03-23 01:32:56 +0100

    Delete Optimal_Control_Example1/Example1_Automatic_differentiation/Reach_target2.m
    junk file(function). Not used in the optimisation task

--- Optimal_Control_Example1/Example1_Automatic_differentiation/Reach_target2.m
+++
@@ -1,9 +0,0 @@
-function [Rt,Jac]=Reach_target2(Utar,U)
-%This "Target-pursuit" quantifies the final the distance between the end
-%point and the specified target point.
-%Target point
-
-
-%Distance between Target point and the current end point.
-Rt=norm(Utar-U);
-Jac=(U-Utar)/Rt;
